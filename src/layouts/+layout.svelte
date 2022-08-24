0fc23535bbaeae536317daa51358d7f15c6c0faa
nc <nelson.ncdesigns@gmail.com>
2022-08-24 13:21:08 -0400

    Init project folders

---
+++ src/layouts/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+import Nav from "$lib/nav.svelte";
+</script>
+<Nav></Nav>
+<slot><!-- optional fallback --></slot>
+
+<style>
+
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+import Layout from "../layouts/+layout.svelte";
+</script>
+
+<Layout>
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</Layout>
+
+<style>
+    h1{
+        color:red;
+    }
+</style>
