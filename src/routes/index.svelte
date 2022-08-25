d7f769c06e0c9a94fc295407d97fdf3dd69d5a96
nc <nelson.ncdesigns@gmail.com>
2022-08-24 23:54:10 -0400

    Commit before vite migration:

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,12 @@
 <script>
-import Layout from "../layouts/+layout.svelte";
-</script>
-
-<Layout>
-    <h1>Welcome to SvelteKit</h1>
-    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-</Layout>
-
-<style>
-    h1{
-        color:red;
-    }
-</style>
+    import Layout from "../layouts/+layout.svelte";
+    </script>
+    
+    <Layout>
+        <h1>Welcome to SvelteKit</h1>
+        <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    </Layout>
+    
+    <style>
+    </style>
+    
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  prefix: 'tw-',
-  plugins: [],
-}
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				blue: {
+					hex: '#25408f'
+				},
+				orange: {
+					hex: '#c26f1f'
+				}
+			},
+			fontSize: {
+				xs: '0.5rem',
+				md: '1.6rem',
+				lg: '3.2rem'
+			}
+		}
+	},
+	prefix: 'tw-',
+	plugins: []
+};
